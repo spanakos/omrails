184be622bfbcbae0aa0bd253268bebd2d12d3232
Crystal Spanakos <crystal@spanakos.com>
2013-06-11 12:40:53 -0400

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Omrails::Application.routes.draw do
 
   devise_for :users
 
-root :to => 'pages#home'
+root :to => 'pins#index'
 get 'about' => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
