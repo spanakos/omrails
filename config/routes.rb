9fe54113379994eae46e17988b5227d8078ca2a9
Crystal Spanakos <crystal@spanakos.com>
2013-06-12 01:57:23 -0400

    Add User show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Omrails::Application.routes.draw do
+  get "users/show"
+
   resources :pins
 
-
   devise_for :users
+  match 'users/:id' => 'users#show', as: :user
 
-root :to => 'pins#index'
-get 'about' => 'pages#about'
+  root :to => 'pins#index'
+  get 'about' => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
